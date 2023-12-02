e05c447cf0b2296a1f42c5998bbdbbe3dbed138b
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2023-12-02 20:35:51 +0900

    creating expense getting function

--- expense_tracker.py
+++ expense_tracker.py
@@ -9,20 +9,36 @@ def main():
 
   # read file and summarize all the expense 
   summarize_expenses()
-  pass
+
 
 
 def get_user_expense():
   print(f'🎯 Getting User Expense')
-  pass
+  expense_name = input('Enter expense name: ')
+  expense_amount = float(input('Enter expense amount: '))
+  # print(f'You have entered {expense_name}, {expense_amount}')
+  
+  #list of categories
+  expense_categories = ['🍔 Food', '🏠 Home', '💼 Work', '🎉 Fun', '⺟ Others']
+
+  #user selects a category
+  while True:
+    print('Select a category: ') 
+    for index, value in enumerate(expense_categories):
+      print(f'   {index+1}. {value}')
+
+    value_range = f'[1 - {len(expense_categories)}]'
+    selected_category = input(f'Enter a category number {value_range}: ')
+    break
+
 
 def save_expense_to_file():
   print(f'🎯 Saving Expense to File')
-  pass
+
 
 def summarize_expenses():
   print(f'🎯 Summarize Expenses')
-  pass
+
 
 #when we run this as a file, below condition will be true
 if __name__ == '__main__':

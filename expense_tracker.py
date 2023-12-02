ac41476de33e3a7cbd61fc05583a4a1d4da4671e
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2023-12-02 20:12:56 +0900

    initializing the main project structure

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,7 +1,28 @@
 def main():
   print(f'🎯 Running Expense Tracker')
+
+  # get user input for expense
+  get_user_expense()
+
+  # write the expense to a file
+  save_expense_to_file()
+
+  # read file and summarize all the expense 
+  summarize_expenses()
   pass
 
+
+def get_user_expense():
+  print(f'🎯 Getting User Expense')
+  pass
+
+def save_expense_to_file():
+  print(f'🎯 Saving Expense to File')
+  pass
+
+def summarize_expenses():
+  print(f'🎯 Summarize Expenses')
+  pass
 
 #when we run this as a file, below condition will be true
 if __name__ == '__main__':
